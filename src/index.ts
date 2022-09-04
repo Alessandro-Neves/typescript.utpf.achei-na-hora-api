82d4f3fbc9c3d814f2032f3a19e1be058f8fcec1
alessandro <git.aleneves@gmail.com>
2022-09-04 16:53:28 -0300

    LF-005: Add cors, config cors, config not-found path

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, Request, Response } from 'express'
+import cors from 'cors'
 
 const app: Express = express();
 
+app.use(cors({
+  origin: ['http: //localhost:3000']
+}));
+
+app.get('/', (req: Request, res: Response) => res.send('Ping success!'))
+
+app.use((req, res) => res.send("Esta rota não existe!").status(404))
+
 app.listen(3030, () => {
   console.log("Servidor rodando na porta 3030 !")
-})
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Ping success!')
 })
 
 // One Year Later
