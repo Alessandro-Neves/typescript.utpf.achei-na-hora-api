572bdbb26465a551a04947a73ef04f2c2e3d29c5
alessandro <git.aleneves@gmail.com>
2022-09-09 15:18:15 -0300

    Queries database e console colors

--- tests.ts
+++ tests.ts
@@ -1,22 +1,24 @@
 // npx ts-node tests.ts
 //https://www.prisma.io/docs/getting-started/setup-prisma/add-to-existing-project/relational-databases/querying-the-database-typescript-postgres
+
 import { PrismaClient } from '@prisma/client'
+import { CError, CSuccess } from './src/tools/console'
 
 const prisma = new PrismaClient()
 
 async function test() {
-  await prisma.user.create({
-    data: {
-      name: 'Alice',
-      email: 'alice@prisma.io',
-      Post: {
-        create: { title: 'Hello World' },
-      },
-      Profile: {
-        create: { bio: 'I like turtles' },
-      },
-    },
-  })
+  // await prisma.user.create({
+  //   data: {
+  //     name: 'Alice',
+  //     email: 'alice@prisma.io',
+  //     Post: {
+  //       create: { title: 'Hello World' },
+  //     },
+  //     Profile: {
+  //       create: { bio: 'I like turtles' },
+  //     },
+  //   },
+  // })
 
   const allUsers = await prisma.user.findMany({
     include: {
@@ -24,7 +26,13 @@ async function test() {
       Profile: true,
     },
   })
-  console.dir(allUsers, { depth: null })
+
+  if(allUsers[0].email == 'alice@prisma.io'){
+    CSuccess('[ OK ]');
+    console.table(allUsers)
+    // console.dir(allUsers, { depth: null })
+  } else CError('[ Error on user::findMany ]')
+
 }
 
 test()
