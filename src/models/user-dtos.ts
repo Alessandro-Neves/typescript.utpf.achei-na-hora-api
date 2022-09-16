33a1814a8a8df271d35fee4f05b53152d213ffcb
alessandro <git.aleneves@gmail.com>
2022-09-15 23:19:08 -0300

    LF-002: Adicionando create person

--- src/models/user-dtos.ts
+++ src/models/user-dtos.ts
@@ -1,4 +1,7 @@
 export type UserCreateRequestDTO = {
   email: string,
   password: string,
+  fullName: string,
+  nickName: string,
+  campus: string,
 }
--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -1,13 +1,26 @@
 import { Prisma } from '../database'
-import { prisma, User } from "@prisma/client";
+import { prisma, User, Person } from "@prisma/client";
 import { ConsoleBlue, ConsoleError, ConsoleSuccess, ConsoleWarn } from "../tools/console";
 import { ExceptionResponse, SimpleResponse } from "../models/default-response-dtos";
 import { UserCreateRequestDTO } from '../models/user-dtos';
 
+
+
 class UserService {
+   private async createUserDB(email: string, password: string): Promise<User> {
+      return await Prisma.user.create({
+         data: {
+            email,
+            password,
+            updated_at: new Date()
+         }
+      })
+   }
+
    public async createUser(dto: UserCreateRequestDTO): Promise<[number, (SimpleResponse | ExceptionResponse)]> {
 
       let user: User | null
+      let person: Person | null
       var response: SimpleResponse | ExceptionResponse
 
       function handleException(status: number, msg: string, error: boolean): [number, ExceptionResponse] {
@@ -39,20 +52,33 @@ class UserService {
       }
 
       try {
-         user = await Prisma.user.create({
-            data: {
-               email: dto.email,
-               password: dto.password,
-               updated_at: new Date()
-            }
-         })
+         user = await this.createUserDB(dto.email, dto.password)
       }catch(error){
          return handleException(500, 'INTERNAL_SERVER_ERROR: criar usuário', true)
       }
+   
+      try {
+         person = await Prisma.person.create({
+            data: {
+               user_id: user.id,
+               full_name: dto.fullName,
+               nickname: dto.nickName,
+               campus: dto.campus,
+               image: null,
+               updated_at: new Date(),
+            }
+         })
+      }catch(error){
+         return handleException(500, 'INTERNAL_SERVER_ERROR: criar pessoa', true)
+      }
+
+
 
       ConsoleBlue(`[ Success operation on AuthService::login at email: ${dto.email} ]`); 
       return [201, { msg: `Usuário com email '${user.email}' criado com sucesso !` }]
    }
+
 }
+
 
 export default new UserService()
