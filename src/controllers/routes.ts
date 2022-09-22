172fc759234397845c5552d85066082f08c2607c
alessandro <git.aleneves@gmail.com>
2022-09-21 23:50:33 -0300

    LF-13: Implementado upload imagens

--- src/controllers/routes.ts
+++ src/controllers/routes.ts
@@ -3,11 +3,13 @@ import { Router } from 'express'
 import authController from './auth-controller'
 import userController from './user-controller'
 import objectController from './object-controller'
+import { uploadController } from './upload-controller'
 
 const routes = Router()
 
 routes.use('/auth', authController)
 routes.use('/user', userController)
 routes.use('/object', objectController)
+routes.use('/upload', uploadController)
 
 export default routes
--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,46 @@ import express, { Express, Request, Response } from 'express'
 import cors from 'cors'
 
 import routes from './controllers/routes';
-import { ConsoleHighlight } from './tools/console';
+import { ConsoleHighlight } from './tools/console'
+
+import multer from 'multer';
+import path from 'path'
 
 const app: Express = express();
 
-app.use(cors({
-  origin: ['http: //localhost:3000']
-}));
+// app.use(cors({origin: ['http: //localhost:3000', 'http: //localhost:3030']}));
+
+app.use(cors())
+
 app.use(express.json())
+express.urlencoded({ extended: true })
 
 app.get('/', (req: Request, res: Response) => res.send('Ping success!'))
 
 app.use(routes)
+
+
+// const multerMiddleware = multer({ dest: 'files/' })
+const multerMiddleware = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {cb(null, 'files/')},
+    filename: function (req, file, cb) {cb(null, new Date().valueOf() + '_' + file.originalname)}
+  })
+})
+app.post('/uploadv2', multerMiddleware.single('image'), function (req, res, next) {
+  // req.file is the `avatar` file
+  // req.body will hold the text fields, if there were any
+  res.status(201).json({msg: 'uploaded!', file: req.file})
+})
+
+app.get('/files/:nameAndExtension', (req: Request, res: Response) => {
+  const dirname = path.resolve()
+  const absoluteFilePath = path.join(dirname, 'files/'+req.params.nameAndExtension)
+
+  console.log('request to '+absoluteFilePath)
+
+  res.sendFile(absoluteFilePath)
+})
 
 app.use((req, res) => res.status(404).send('Esta rota não existe!'))
 
